b9fb8b17b9a3abe2261887ed8d0606e25de24cdd
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-05-27 11:18:36 +0500

    Fitrana with wheat done

--- app/src/main/java/com/example/mc_zakatcalculator/WheatActivity.java
+++ app/src/main/java/com/example/mc_zakatcalculator/WheatActivity.java
@@ -3,12 +3,32 @@ package com.example.mc_zakatcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WheatActivity extends AppCompatActivity {
 
+    private Button submitbtn;
+    private EditText prices,member;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wheat);
+        submitbtn=findViewById(R.id.wheatcalculate);
+        prices=findViewById(R.id.price);
+        member=findViewById(R.id.members);
+        textView=findViewById(R.id.textView);
+        submitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float pricee=Float.parseFloat(prices.getText().toString());
+                int members=Integer.parseInt(member.getText().toString());
+                float val=pricee*2*members;
+                textView.setText(Float.toString(val));
+            }
+        });
     }
 }
